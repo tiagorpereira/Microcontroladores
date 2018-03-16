8425bd654c03ce078be37c9e4e40cca270091459
Tiago Rodrigues Pereira <tiagorodriguesp2@gmail.com>
2018-03-16 15:49:37 -0300

    Aula 4

--- 4_Testes_C_files/Aula3_Q1.c
+++ 4_Testes_C_files/Aula3_Q1.c
@@ -1,16 +1,24 @@
-#include <msp430fr2433>
+#include <msp430.h>
 #define LED1 BIT0
-#define LED2 BIT6
+#define LED2 BIT1
 
-void main(void)
-{
-	int count = 0;
-	WDTCTL = WDTPW | WDTHOLD;
-	PIDIR = LED1 + LED2;
-	PIOUT = 0;
-	for(;;)
-			{
-				PIOUT ^= (LED1 + LED2);
-				for(count = 0, count < "valor", count++){};
-			};
+void main(void) {
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+                                            // to activate previously configured port settings
+    volatile unsigned int i;            // volatile to prevent optimization
+
+  	P1DIR = LED1 + LED2;
+  	P1OUT = ~(LED1 + LED2);
+  	i = 32768;
+
+    while(1) {
+
+        P1OUT = ~(LED1 + LED2);
+        do i--;
+        while(i != 0);
+	    P1OUT = LED1 + LED2;
+	    do i--;
+	    while(i != 0);
+    };
 }
--- 4_Testes_C_files/Aula3_Q3.c
+++ 4_Testes_C_files/Aula3_Q3.c
@@ -1,23 +1,31 @@
-#include <msp430fr2433>
+#include <msp430>
 #define LED1 BIT0
-#define LED2 BIT6
-#define BTN BIT2
+#define LED2 BIT1
+#define BTN BIT3
+
 void main(void)
 {
-	int count = 0;
-	WDTCTL = WDTPW | WDTHOLD;
-	PIDIR = LED1 + LED2;
-	PIOUT = 0;
-	for(;;)
+	WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+	PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+																					// to activate previously configured port settings
+	volatile unsigned int i;            // volatile to prevent optimization
+
+	P1DIR = LED1 + LED2;
+	P1OUT = ~(LED1 + LED2);
+	i = 32768;
+
+	while(1)
 			{
         if(PIOUT & BTN == 0){
-          for(count = 0, count < "valor", count++){};
-          PIOUT = 0;
-          for(count = 0, count < "valor", count++){};
+					P1OUT = ~(LED1 + LED2);
+					do i--;
+					while( i != 0);
           PIOUT = LED1 + LED2;
-          for(count = 0, count < "valor", count++){};
-          PIOUT = 0;
-          for(count = 0, count < "valor", count++){};
+					do i--;
+					while( i != 0);
+					P1OUT = ~(LED1 + LED2);
+					do i--;
+					while( i != 0);
           PIOUT = LED1 + LED2;
         };
 			};
